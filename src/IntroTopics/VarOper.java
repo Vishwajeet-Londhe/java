6333a07bb3fb9ae0da2adb0f2bea2fea972184ec
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2025-01-19 21:58:11 +0530

    var operations

---
+++ src/IntroTopics/VarOper.java
@@ -0,0 +1,17 @@
+package IntroTopics;
+
+public class VarOper {
+    public static void main(String[] args) {
+        System.out.println("hello");
+
+        int val =  10;
+        System.out.println(val);
+
+        System.out.println(5);
+
+        System.out.println("I am" + " ok okk");
+
+        System.out.println("Number = " + val);
+
+    }
+}
