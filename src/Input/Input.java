8a0872ade7d0e284f9bfc37afab39a88dede87d0
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2025-01-19 22:09:32 +0530

    input

--- src/Input/Input.java
+++ src/Input/Input.java
@@ -1,4 +1,18 @@
 package Input;
 
+import java.util.Scanner;
+
 public class Input {
+    public static void main(String[] args) {
+
+        //create an object of scanner Class
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter an integer value : ");
+
+        //take input from user
+        int number = input.nextInt();
+
+        System.out.println("You entered "  +  number);
+    }
 }
