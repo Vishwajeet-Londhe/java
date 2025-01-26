6a55a86708a1aed8583a172f810042af4c05f5a3
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2025-01-26 20:51:12 +0530

    performed arithmetic

---
+++ src/Operator/Arithmetic.java
@@ -0,0 +1,14 @@
+package Operator;
+
+public class Arithmetic {
+    public static void main(String[] args) {
+
+        int a=12, b=5;
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+}
