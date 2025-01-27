b27dc73d39b82b957855110ac36e9000d0079302
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2025-01-27 19:54:10 +0530

    logical and relational

---
+++ src/Operator/Relational.java
@@ -0,0 +1,15 @@
+package Operator;
+
+public class Relational {
+    public static void main(String[] args) {
+        int a=5, b=20;
+        System.out.println(a+" "+b);
+
+        System.out.println(a==b);
+        System.out.println(a!=b);
+        System.out.println(a>b);
+        System.out.println(a<b);
+        System.out.println(a>=b);
+        System.out.println(a<=b);
+    }
+}
---
+++ src/Operator/logical.java
@@ -0,0 +1,9 @@
+package Operator;
+
+public class logical {
+    public static void main(String[] args) {
+        System.out.println((5>3) && (5<7));
+        System.out.println((5>3) || (5<7));
+        System.out.println(!(5>3));
+    }
+}
