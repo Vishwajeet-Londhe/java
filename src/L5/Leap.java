66f0defbd7e6cdc66f7ea15cfbfd2a14b6af1ec3
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2025-01-28 23:19:52 +0530

    leap done

--- src/L5/Leap.java
+++ src/L5/Leap.java
@@ -1,7 +1,17 @@
 package L5;
 
+import java.util.Scanner;
+
 public class Leap {
     public static void main(String[] args) {
 
+        Scanner scn = new Scanner(System.in);
+        int year = scn.nextInt();
+
+        if (((year%4 == 0) && (year%100 != 0)) || (year%400 == 0)){
+            System.out.println("Leap Year");
+        }else {
+            System.out.println("Not a Leap Year");
+        }
     }
 }
