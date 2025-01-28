094d3a65da65a86badb4fcc223b74a28d9fbe75f
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2025-01-28 20:23:00 +0530

    swich

---
+++ src/L5/Switch.java
@@ -0,0 +1,49 @@
+package L5;
+
+public class Switch {
+    public static void main(String[] args) {
+
+//        int number = 29;
+//        String size;
+//
+//        switch(number){
+//            case 29:
+//                size = "small";
+//                break;
+//
+//            case 42:
+//                size = "medium";
+//                break;
+//
+//            case 48:
+//                size = "large";
+//                break;
+//
+//            case 50:
+//                size = "huge";
+//                break;
+//
+//            default:
+//                size = "unknown";
+//                break ;
+//        }
+//        System.out.println("Size : " + size);
+
+
+        int exp = 1;
+
+        switch (exp){
+            case 1:
+                System.out.println("1");
+
+            case 2:
+                System.out.println("2");
+
+            case 3:
+                System.out.println("3");
+
+            default:
+                System.out.println("Default");
+        }
+    }
+}
