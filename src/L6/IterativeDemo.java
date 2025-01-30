7b2217f62114117e93ce402c07f06082035e4f39
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2025-01-31 00:31:34 +0530

    iterative demo

---
+++ src/L6/IterativeDemo.java
@@ -0,0 +1,17 @@
+package L6;
+
+public class IterativeDemo {
+    public static void main(String[] args) {
+//        System.out.println(1);
+//        System.out.println(2);
+//        System.out.println(3);
+//        System.out.println(4);
+//        System.out.println(5);
+
+        int n =100;
+
+        for(int i =1; i<=n; i++){
+            System.out.println("Hello Akku" + " " + i);
+        }
+    }
+}
