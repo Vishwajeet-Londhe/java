9a887d842fa7bb6fe0c0d0b533dc56e69c14d1c3
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2025-02-03 23:39:57 +0530

    second M

--- src/L6/Sum.java
+++ src/L6/Sum.java
@@ -2,7 +2,7 @@ package L6;
 
 public class Sum {
     public static void main(String[] args) {
-        //sum of n natural number from 1 to 1000
+        //sum of n natural number from 1 to 100
 
         int sum =0;
 
@@ -10,5 +10,15 @@ public class Sum {
             sum +=i;
         }
         System.out.println(sum);
+
+        //sum of n natural number from 1 to 1000 ( M-2 )
+
+        int total =0;
+        int n = 1000;
+
+        for(int i = 1; i<=n; i++){
+            total +=i;
+        }
+        System.out.println(total);
     }
 }
