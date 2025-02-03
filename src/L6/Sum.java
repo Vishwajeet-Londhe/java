0ac7a5ff79c63b2da4683e3c1037baa43501a783
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2025-02-03 23:33:14 +0530

    sum of numbers

---
+++ src/L6/Sum.java
@@ -0,0 +1,14 @@
+package L6;
+
+public class Sum {
+    public static void main(String[] args) {
+        //sum of n natural number from 1 to 1000
+
+        int sum =0;
+
+        for(int i = 1; i<=100; i++){
+            sum +=i;
+        }
+        System.out.println(sum);
+    }
+}
