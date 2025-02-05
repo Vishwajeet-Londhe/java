b1e4a4df24878cfac7c4974f0f83c65558e9405e
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2025-02-05 21:43:31 +0530

    continue ex

--- src/L7/Continue.java
+++ src/L7/Continue.java
@@ -3,11 +3,27 @@ package L7;
 public class Continue {
     public static void main(String[] args) {
 
-        for(int i = 1; i<=10; i++){
-            if(i>4 && i<9){
-                continue;
+//        for(int i = 1; i<=10; i++){
+//            if(i>4 && i<9){
+//                continue;
+//            }
+//            System.out.println(i);
+//        }
+
+        int i =1 , j=1;
+
+        while (i<=3){
+            System.out.println("outer loop" + i );
+
+            while (j<=3){
+                if(j==2){
+                    j++;
+                    continue;
+                }
+                System.out.println("Innner loop" + j );
+                j++;
             }
-            System.out.println(i);
+            i++;
         }
     }
 }
