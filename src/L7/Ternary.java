96d1f17ba6bf6091aed6bde8097968b74f5ff9ab
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2025-02-05 21:53:21 +0530

    ternary opr

---
+++ src/L7/Ternary.java
@@ -0,0 +1,12 @@
+package L7;
+
+public class Ternary {
+    public static void main(String[] args) {
+
+        int marks = 75;
+
+        String result =  (marks > 70) ? "Pass" : "fail" ;
+
+        System.out.println(result);
+    }
+}
