339ed5ff12be5cdb9cb1107e0585fed4e262457b
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2025-02-09 23:46:40 +0530

    manually added in byte

--- src/L7/TypeCasting.java
+++ src/L7/TypeCasting.java
@@ -4,37 +4,52 @@ public class TypeCasting {
     public static void main(String[] args) {
 
         //case1
+
         byte by = 10;
         short sh = 10;
         int in = 10;
         long lo = 10;
 
 //        by = sh;
-//        by= in;
-//        by=lo;
+//        by = in;
+//        by = lo;
 
-        sh=by;
+        sh = by;
+//        sh = in;
+//        sh = lo;
 
-//        sh=in;
-//        sh=lo;
+        in = by;
+        in = sh;
+//        in = lo;
 
-        in=by;
-        in=sh;
+        lo = by;
+        lo = sh;
+        lo = in;
 
-//        in=lo;
+        //case 2
 
-        lo=by;
-        lo=sh;
-        lo=in;
+        by = 100;
 
-        //case-2
-
-        by=100;
-
-        for (byte b=0; b<=128; b++){
-            System.out.println("hello");
+        for (byte b = 0; b<=126; b++){
+            System.out.println("typecasting");
         }
 
-        //case3
+        // case 3
+
+        float f = 5.6f;
+        double d = 6.7;
+
+        //f=d;  invalid
+        d=f;
+
+
+        //case 4
+
+        //in = f;       invalid, float is big so
+
+        in = (int)f;
+
+        byte b = (byte) 130;
+        System.out.println(b);
     }
 }
