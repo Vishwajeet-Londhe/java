7b9ab5dd52605174edd7dede6ea2464c9185ae4f
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2025-02-11 20:57:16 +0530

    odd pattern

---
+++ src/L8/PatternOdd.java
@@ -0,0 +1,29 @@
+package L8;
+
+public class PatternOdd {
+    public static void main(String[] args) {
+
+        //number must be odd
+        int n = 5;
+        int trows = n;
+
+        int stars = 1;
+
+        for(int row = 1; row<=trows; row++){
+
+            //work for current
+            for(int cst =1 ; cst<=stars; cst++){
+                System.out.print("*");
+            }
+
+            //pre for next row
+            System.out.println();
+            if (row <= trows/2){
+                stars ++;
+            }else {
+                stars --;
+            }
+        }
+
+    }
+}
