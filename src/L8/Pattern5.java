2db871a19fc600c2e8024afa259a978526abcea4
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2025-02-12 14:16:07 +0530

    first row P5

---
+++ src/L8/Pattern5.java
@@ -0,0 +1,15 @@
+package L8;
+
+public class Pattern5 {
+    public static void main(String[] args) {
+
+        int n = 4;
+
+        //print the first row
+
+        for (int frow = 1; frow<=2*n-1; frow++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+}
