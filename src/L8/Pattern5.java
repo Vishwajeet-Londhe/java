9a5c16984d217c1b55002cdc42b219e759ab2445
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2025-02-12 19:23:53 +0530

    pattern 5

--- src/L8/Pattern5.java
+++ src/L8/Pattern5.java
@@ -3,7 +3,7 @@ package L8;
 public class Pattern5 {
     public static void main(String[] args) {
 
-        int n = 4;
+        int n = 14;
 
         //print the first row
 
@@ -11,5 +11,32 @@ public class Pattern5 {
             System.out.print("*");
         }
         System.out.println();
+
+        int trows = n-1;
+        int stars = n-1;
+        int spaces = 1;
+
+        for (int row = 1; row<=trows; row++){
+            //work
+
+            //star
+            for (int cst = 1; cst<=stars; cst++){
+                System.out.print("*");
+            }
+            //spaces
+            for (int csp = 1; csp<=spaces; csp++){
+                System.out.print(" ");
+            }
+            //star
+            for (int cst = 1; cst<=stars; cst++){
+                System.out.print("*");
+            }
+
+            //prep
+            System.out.println();
+            stars--;
+            spaces +=2;
+
+        }
     }
 }
