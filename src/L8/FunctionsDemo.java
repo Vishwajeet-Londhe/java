8030c794bfb9b48c5a801e0d43869d83cb3e6b6c
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2025-02-13 15:31:12 +0530

    add by fuc para

--- src/L8/FunctionsDemo.java
+++ src/L8/FunctionsDemo.java
@@ -8,11 +8,17 @@ public class FunctionsDemo {
         //calling a function
         greetingCustomers();
         System.out.println("see you again");
+        addNumbers(50,5);
     }
 
     //Declaring a function
     public static void greetingCustomers(){
         System.out.println("Hello to You!");
         System.out.println("Hope You're having a good day");
+    }
+
+    public static void addNumbers(int val1, int val2){
+        int sum = val1 + val2;
+        System.out.println(sum);
     }
 }
