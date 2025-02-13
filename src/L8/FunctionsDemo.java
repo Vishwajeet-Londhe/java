33e15059bdb841da1e96c76d9ca268144e309987
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2025-02-14 02:38:14 +0530

    M2 return type

--- src/L8/FunctionsDemo.java
+++ src/L8/FunctionsDemo.java
@@ -1,6 +1,33 @@
 package L8;
 
 public class FunctionsDemo {
+
+    //M-1
+//    public static void main(String[] args) {
+//
+//        System.out.println("Let's get started");
+//
+//        //calling a function
+//        greetingCustomers();
+//        System.out.println("see you again");
+//        addNumbers(50,17);
+//        addNumbers(43,17);
+//        addNumbers(50,1);
+//    }
+//
+//    //Declaring a function
+//    public static void greetingCustomers(){
+//        System.out.println("Hello to You!");
+//        System.out.println("Hope You're having a good day");
+//    }
+//
+//    public static void addNumbers(int val1, int val2){
+//        int sum = val1 + val2;
+//        System.out.println(sum);
+//    }
+
+    //M-2
+
     public static void main(String[] args) {
 
         System.out.println("Let's get started");
@@ -8,7 +35,9 @@ public class FunctionsDemo {
         //calling a function
         greetingCustomers();
         System.out.println("see you again");
-        addNumbers(50,5);
+
+        int res = addNumbers(25,10);
+        System.out.println(res);
     }
 
     //Declaring a function
@@ -17,8 +46,8 @@ public class FunctionsDemo {
         System.out.println("Hope You're having a good day");
     }
 
-    public static void addNumbers(int val1, int val2){
+    public static int addNumbers(int val1, int val2){
         int sum = val1 + val2;
-        System.out.println(sum);
+        return sum;
     }
 }
