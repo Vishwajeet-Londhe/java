d6663a85bf9cfde0522b3ae5bcc7039190da53fd
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2025-02-14 03:43:00 +0530

    4 para

--- src/L8/FunctionsDemo.java
+++ src/L8/FunctionsDemo.java
@@ -36,7 +36,7 @@ public class FunctionsDemo {
         greetingCustomers();
         System.out.println("see you again");
 
-        int res = addNumbers(25,10);
+        int res = addNumbers(25,10,10,10);
         System.out.println(res);
     }
 
@@ -46,8 +46,8 @@ public class FunctionsDemo {
         System.out.println("Hope You're having a good day");
     }
 
-    public static int addNumbers(int val1, int val2){
-        int sum = val1 + val2;
+    public static int addNumbers(int val1, int val2, int val3, int val4){
+        int sum = val1 + val2 + val3 + val4;
         return sum;
     }
 }
