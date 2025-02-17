9386041b477f534cae8518eed45176bc532bbee8
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2025-02-18 02:50:38 +0530

    Armstrong TF

---
+++ src/L8/ArmstrongNumber.java
@@ -0,0 +1,32 @@
+package L8;
+
+public class ArmstrongNumber {
+    public static void main(String[] args) {
+        System.out.println(isArmstrong(153));
+    }
+
+    public static boolean isArmstrong(int n){
+
+        int nod = countDigits(n);
+        int on = n;
+        int sum = 0;
+        while (n>0){
+            int rem = n%10;
+            sum += Math.pow(rem,nod);
+            n=n/10;
+        }
+        return sum==on;
+    }
+
+    public static int countDigits(int n){
+        int nod = 0;
+
+        while (n>0){
+            n=n/10;
+            nod ++;
+        }
+        return nod;
+    }
+
+
+}
