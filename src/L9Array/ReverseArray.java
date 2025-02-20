22fd864eef7f9702a4e8c880c325bc712696588f
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2025-02-20 20:47:33 +0530

    reverse array

---
+++ src/L9Array/ReverseArray.java
@@ -0,0 +1,32 @@
+package L9Array;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+        int [] arr = {10,20,30,40,50};
+
+        display(arr);
+        reverse(arr);
+        display(arr);
+    }
+
+    public static void display(int[] arr){
+        for (int i =0;i< arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
+
+    public static void reverse(int[] arr){
+
+        int i =0;
+        int j = arr.length-1;
+
+        while (i<=j){
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+
+            i++;
+            j--;
+        }
+    }
+}
