2413cdd2bf9941e728f8a7126c95a77b68b269e9
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2025-02-23 23:57:48 +0530

    array rot ex

--- src/L9Array/RotateArray.java
+++ src/L9Array/RotateArray.java
@@ -2,6 +2,16 @@ package L9Array;
 
 public class RotateArray {
     public static void main(String[] args) {
-
+//        k=-2   3 4 1 2
+//        k=-1   2 3 4 1
+//        k=0    1 2 3 4
+//        k=1    2 3 4 1
+//        k=2    3 4 1 2
+//        k=3    4 1 2 3
+//        k=4    1 2 3 4
+//        k=5    4 1 2 3
+//        k=6    3 4 1 2
+//        k=7    2 3 4 1
+//        k=8    1 2 3 4
     }
 }
