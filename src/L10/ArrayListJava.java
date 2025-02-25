9cfe6a78ba07038c91dc7c90bf442ee19b120016
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2025-02-25 16:20:45 +0530

    replace element by other

--- src/L10/ArrayListJava.java
+++ src/L10/ArrayListJava.java
@@ -8,11 +8,22 @@ public class ArrayListJava {
 //        ArrayList<Integer> arrayList = new ArrayList<>();
         ArrayList<String> languages = new ArrayList<>();
 
+        //add elements
         languages.add("vishwajeet");
         languages.add("Vaibhavi");
         languages.add("Sahil");
         languages.add(1,"Shivam");
 
+        System.out.println(languages);
+
+        //access
+
+        String str = languages.get(1);
+        System.out.println(str);
+
+        //change
+
+        languages.set(2,"ketaki");
         System.out.println(languages);
     }
 }
