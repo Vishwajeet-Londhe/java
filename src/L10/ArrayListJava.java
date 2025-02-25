f76943545fa08c3977b0a6521842eb98a057fabc
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2025-02-25 16:08:49 +0530

    add element in arraylist

---
+++ src/L10/ArrayListJava.java
@@ -0,0 +1,17 @@
+package L10;
+
+import java.util.ArrayList;
+
+public class ArrayListJava {
+    public static void main(String[] args) {
+
+//        ArrayList<Integer> arrayList = new ArrayList<>();
+        ArrayList<String> languages = new ArrayList<>();
+
+        languages.add("vishwajeet");
+        languages.add("Vaibhavi");
+        languages.add("Sahil");
+
+        System.out.println(languages);
+    }
+}
