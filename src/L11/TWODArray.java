d9d40dd3d434deae1bbfb4542ac9560e8b5222f5
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2025-02-28 00:22:29 +0530

    print ele from arr

--- src/L11/TWODArray.java
+++ src/L11/TWODArray.java
@@ -29,5 +29,20 @@ public class TWODArray {
 
         a[2][3] = 150;
         System.out.println(a[2][3]); //updated the value
+
+        //print all values from array
+
+        int [][] a2 = {
+                {1,2,7},
+                {3,4,8},
+                {5,6,9}
+        };
+
+        System.out.println("-----prin all ele from arr-----");
+        for (int i =0; i<a2.length;i++){
+            for (int j =0; j<a2[0].length;j++){
+                System.out.println(a2[i][j]);
+            }
+        }
     }
 }
