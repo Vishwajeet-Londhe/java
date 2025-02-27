681861026540dfc0b32226ec34a4caa10f32c56e
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2025-02-27 23:15:00 +0530

    2D Arr ini

---
+++ src/L11/TWODArray.java
@@ -0,0 +1,14 @@
+package L11;
+
+public class TWODArray {
+    public static void main(String[] args) {
+
+        int [] [] a = new int[3][4];
+
+        int [] [] a1 = {
+                {1,2},
+                {3,4},
+                {5,6}
+        };
+    }
+}
