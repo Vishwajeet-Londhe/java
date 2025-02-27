852e3d9ef865e2b9010d5140a5396b8a9a301ab4
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2025-02-28 03:06:44 +0530

    print ele from jagged arr

--- src/L11/TWODArray.java
+++ src/L11/TWODArray.java
@@ -76,5 +76,14 @@ public class TWODArray {
         System.out.println(arr[0].length);
         System.out.println(arr[1].length);
         System.out.println(arr[2].length);
+
+        //printing all element from jaggged array
+
+        for (int i=0;i<arr.length;i++){
+            for (int j=0;j<arr[i].length;j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
     }
 }
