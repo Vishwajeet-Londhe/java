8fd8d8032cc56c3d388e7a27846e407d0cc87d6a
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2025-03-18 22:18:44 +0530

    ex on string comparision

--- src/L12String/StringDemo.java
+++ src/L12String/StringDemo.java
@@ -95,5 +95,22 @@ public class StringDemo {
         System.out.println(besti.substring(0,6));
         System.out.println(besti.substring(1));
 
+        //some more operations
+
+        System.out.println(besti.lastIndexOf('a'));
+        System.out.println(besti.startsWith("Hemali"));
+        System.out.println(besti.startsWith("hemali"));
+
+        //ex
+        String s1 = "hello";
+        String s2 = s1;
+        String s3 = "hello";
+        String s4 = new String("hello");
+
+        System.out.println((s1==s2)+ "," + s1.equals(s2));
+        System.out.println((s2==s3)+ "," + s1.equals(s3));
+        System.out.println((s1==s4)+ "," + s1.equals(s4));
+
+
     }
 }
