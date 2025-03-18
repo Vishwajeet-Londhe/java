397c4afb6d18ee3819e86e881436105032b509a9
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2025-03-18 23:27:44 +0530

    isPalindrome

---
+++ src/Leetcode/isPalindrome.java
@@ -0,0 +1,22 @@
+package Leetcode;
+
+public class isPalindrome {
+    public static void main (String[] args){
+        System.out.println(isPalindrome(156));
+    }
+    public static boolean isPalindrome(int x) {
+
+        int temp = x;
+        int rev = 0;
+
+        while(x>0){
+            int digit = x%10;
+            rev = (rev*10)+digit;
+            x=x/10;
+        }
+        if(rev == temp){
+            return true;
+        }
+        return false;
+    }
+}
