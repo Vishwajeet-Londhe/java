c6ed4b33545ea564aad0b162dda024358691b6e3
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2025-03-22 01:03:29 +0530

    more ope on Stock

--- src/Leetcode/Stock122.java
+++ src/Leetcode/Stock122.java
@@ -3,7 +3,12 @@ package Leetcode;
 public class Stock122 {
     public static void main (String[] args){
         int[] prices = {7,1,5,3,6,4};
+        int[] prices1 = {7,6,5,4,3,2,1};
+        int[] prices2 = {1,2,3,4,5};
+
         System.out.println(maxProfit(prices));
+        System.out.println(maxProfit(prices1));
+        System.out.println(maxProfit(prices2));
     }
 
     public static int maxProfit(int[] prices) {
