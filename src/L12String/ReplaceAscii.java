96280f3846a992f1ac336ad0c41c7eb602b49dcf
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2025-03-22 19:04:49 +0530

    Replace Ascii using SB

---
+++ src/L12String/ReplaceAscii.java
@@ -0,0 +1,24 @@
+package L12String;
+
+public class ReplaceAscii {
+    public static void main(String[] args) {
+        System.out.println(ReplaceAsciiFunction("cdEFG"));
+    }
+
+    public static String ReplaceAsciiFunction(String str){
+        StringBuilder sb = new StringBuilder(str);
+
+        for (int i=0; i< sb.length();i++){
+            char ch = sb.charAt(i);
+
+            if(i%2 == 0){
+                ch =(char) (ch +1);
+                sb.setCharAt(i,ch);
+            }else {
+                ch=(char)(ch-1);
+                sb.setCharAt(i,ch);
+            }
+        }
+        return sb.toString();
+    }
+}
