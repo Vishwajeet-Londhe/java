60a90e7300f1461ebad41f079f5382318ea450c9
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2025-03-26 10:53:11 +0530

    scannet int

---
+++ src/L13Input/UserInput.java
@@ -0,0 +1,16 @@
+package L13Input;
+import java.util.Scanner;
+
+public class UserInput {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter an Interger:");
+        int num = input.nextInt();
+        System.out.println("You Entered:" + num);
+
+
+        input.close();
+    }
+}
