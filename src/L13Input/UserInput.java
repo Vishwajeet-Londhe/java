a903fb0bb5e0e6032d80a294bafc9cadfc750eb5
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2025-03-26 10:55:47 +0530

    scanner string nextLine

--- src/L13Input/UserInput.java
+++ src/L13Input/UserInput.java
@@ -14,6 +14,10 @@ public class UserInput {
         String mystring = input.next();
         System.out.println("You Entered:" + mystring);
 
+        System.out.println("Enter Text:");
+        String mystring1 = input.nextLine();
+        System.out.println("You Entered:" + mystring1);
+
         input.close();
     }
 }
