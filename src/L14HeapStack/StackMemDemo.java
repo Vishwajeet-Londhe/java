2d3a02cf843d6992348fedd60f965f30fdff81a9
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2025-03-26 11:37:15 +0530

    stack ex

---
+++ src/L14HeapStack/StackMemDemo.java
@@ -0,0 +1,15 @@
+package L14HeapStack;
+
+public class StackMemDemo {
+    public static void main(String[] args) {
+        System.out.println("Hello THere !");
+        greetings();
+        askQuery();
+    }
+    public static void greetings(){
+        System.out.println("Hope! You are well");
+    }
+    public static void askQuery(){
+        System.out.println("How may I help you");
+    }
+}
