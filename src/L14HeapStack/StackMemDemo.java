7e9321fd195de96b4f566148329501dc66b8637e
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2025-03-26 11:54:13 +0530

    stack ex2

--- src/L14HeapStack/StackMemDemo.java
+++ src/L14HeapStack/StackMemDemo.java
@@ -1,15 +1,35 @@
 package L14HeapStack;
 
 public class StackMemDemo {
+//    public static void main(String[] args) {
+//        System.out.println("Hello THere !");
+//        greetings();
+//        askQuery();
+//    }
+//    public static void greetings(){
+//        System.out.println("Hope! You are well");
+//    }
+//    public static void askQuery(){
+//        System.out.println("How may I help you");
+//    }
+
+    //ex2
+
     public static void main(String[] args) {
-        System.out.println("Hello THere !");
-        greetings();
-        askQuery();
+        System.out.println("Hello there!");
+
+        int defaultval = 2;
+        defaultval=multiplyFour(defaultval);
+        defaultval=multiplyTwo(defaultval);
+
+        System.out.println(defaultval);
     }
-    public static void greetings(){
-        System.out.println("Hope! You are well");
+    private static int multiplyFour(int val){
+        val *= 4;
+        return val;
     }
-    public static void askQuery(){
-        System.out.println("How may I help you");
+    private static int multiplyTwo(int val){
+        val *= 2;
+        return val;
     }
 }
