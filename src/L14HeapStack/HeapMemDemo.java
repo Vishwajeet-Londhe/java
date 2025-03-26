8ec380299023c6440ef8167cf27dbe2b77e477c8
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2025-03-26 12:17:57 +0530

    heap ex

---
+++ src/L14HeapStack/HeapMemDemo.java
@@ -0,0 +1,15 @@
+package L14HeapStack;
+
+import java.util.ArrayList;
+
+public class HeapMemDemo {
+    public static void main(String[] args) {
+
+        int x = 10;
+
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+    }
+}
