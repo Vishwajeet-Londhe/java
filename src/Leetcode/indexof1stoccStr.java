5484842bce001bb5a79b2c5ccb3cd4237193a088
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2025-03-27 23:16:49 +0530

    indexof1occofSTR

---
+++ src/Leetcode/indexof1stoccStr.java
@@ -0,0 +1,20 @@
+package Leetcode;
+
+public class indexof1stoccStr {
+    public static void main(String[] args) {
+        System.out.println(strStr("sadbutsad", "sad"));
+        System.out.println(strStr("goodamigesami", "ami"));
+    }
+    public static int strStr(String haystack, String needle) {
+
+        for(int i = 0; i<haystack.length()-needle.length()+1;i++){
+
+            if(haystack.charAt(i) == needle.charAt(0)){
+                if(haystack.substring(i,needle.length()+i).equals(needle)){
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+}
