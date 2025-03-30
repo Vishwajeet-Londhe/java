df8cb00d2771d6a93245d77a6696092ec3990cff
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2025-03-30 23:39:43 +0530

    extra ope

--- src/Leetcode/LenOfLastWord58.java
+++ src/Leetcode/LenOfLastWord58.java
@@ -3,10 +3,12 @@ package Leetcode;
 public class LenOfLastWord58 {
     public static void main(String[] args) {
         String s = "Hello World";
-
+        String s1 = "   fly me   to   the moon  ";
+        String s2 = "luffy is still joyboy";
 
         System.out.println(lengthOfLastWord(s));
-
+        System.out.println(lengthOfLastWord(s1));
+        System.out.println(lengthOfLastWord(s2));
     }
     public static int lengthOfLastWord(String s) {
         String str = s.trim();
