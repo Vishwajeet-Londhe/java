c53f74a4865fb2747138f06b311ad2df78659563
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2025-04-02 23:52:53 +0530

    more operations

--- src/Leetcode/isSubsequence392.java
+++ src/Leetcode/isSubsequence392.java
@@ -4,7 +4,20 @@ public class isSubsequence392 {
     public static void main(String[] args) {
         String s = "abc";
         String t = "ahbgdc";
+
+        String s1 = "axc";
+        String t1 = "ahbgdc";
+
+        String s2 = "ace";
+        String t2 = "abecd";
+
+        String s3 = "ace";
+        String t3 = "abzxvcde";
+
         System.out.println(isSubsequence(s,t));
+        System.out.println(isSubsequence(s1,t1));
+        System.out.println(isSubsequence(s2,t2));
+        System.out.println(isSubsequence(s3,t3));
     }
 
     public static boolean isSubsequence(String s, String t) {
