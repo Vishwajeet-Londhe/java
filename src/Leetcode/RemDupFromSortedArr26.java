025362fc0ee91df512c3a09c2d87c05f413d16a4
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2025-04-03 23:14:51 +0530

    more operations

--- src/Leetcode/RemDupFromSortedArr26.java
+++ src/Leetcode/RemDupFromSortedArr26.java
@@ -3,7 +3,13 @@ package Leetcode;
 public class RemDupFromSortedArr26 {
     public static void main(String[] args) {
         int[] nums = {1,1,2};
+        int[] nums1 = {0,0,1,1,1,2,2,3,3,4};
+        int[] nums2 = {5,6,6,6,7,7,7,7,8,9};
+
         System.out.println(removeDuplicates(nums));
+        System.out.println(removeDuplicates(nums1));
+        System.out.println(removeDuplicates(nums2));
+
     }
 
     public static int removeDuplicates(int[] nums) {
