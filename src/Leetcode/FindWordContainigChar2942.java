cadb983669e8d58e6bbff2bce5e09f9e44771e18
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2025-04-03 23:51:00 +0530

    more operation

--- src/Leetcode/FindWordContainigChar2942.java
+++ src/Leetcode/FindWordContainigChar2942.java
@@ -6,7 +6,16 @@ public class FindWordContainigChar2942 {
     public static void main(String[] args) {
         String[] words = {"leet","code"};
         char x = 'e';
+
+        String[] words1 = {"abc","bcd","aaaa","cbc"};
+        char x1 = 'a';
+
+        String[] words2 = {"anu", "vaibhu", "akku", "apurva", "tanu", "vishwajeet"};
+        char x2 = 'v';
+
         System.out.println(findWordsContaining(words,x));
+        System.out.println(findWordsContaining(words1,x1));
+        System.out.println(findWordsContaining(words2,x2));
     }
 
     public static List<Integer> findWordsContaining(String[] words, char x) {
