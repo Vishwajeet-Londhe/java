b0a51f1b706f91a1bc572e002ff6b4209329d9c1
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2025-04-20 20:23:01 +0530

    PrintIncreasing

---
+++ src/L15Recursion/PrintIncreasing.java
@@ -0,0 +1,15 @@
+package L15Recursion;
+
+public class PrintIncreasing {
+    public static void f(int n){
+        if(n==0){
+            return;
+        }
+        f(n-1);
+        System.out.println(n);
+    }
+
+    public static void main(String[] args){
+        f(5);
+    }
+}
