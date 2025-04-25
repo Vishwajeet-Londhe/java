95b347615b5c0c0227614c1eb4ab090ed35693de
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2025-04-25 23:53:30 +0530

    isoMorphicString205

---
+++ src/Leetcode/IsomorphicStrings205.java
@@ -0,0 +1,31 @@
+package Leetcode;
+
+public class IsomorphicStrings205 {
+    public static void main(String[] args) {
+        String s = "egg";
+        String t = "add";
+
+        System.out.println(isIsomorphic(s,t));
+    }
+
+    public static boolean isIsomorphic(String s, String t) {
+        int[] mapST = new int[256]; // s → t
+        int[] mapTS = new int[256]; // t → s
+
+        for (int i = 0; i < s.length(); i++) {
+            char chS = s.charAt(i);
+            char chT = t.charAt(i);
+
+            if (mapST[chS] == 0 && mapTS[chT] == 0) {
+                mapST[chS] = chT;
+                mapTS[chT] = chS;
+            } else {
+                if (mapST[chS] != chT || mapTS[chT] != chS) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
