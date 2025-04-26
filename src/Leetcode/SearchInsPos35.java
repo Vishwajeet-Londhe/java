a933bff70b8082301bab25e4978231d6404b5061
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2025-04-26 23:50:23 +0530

    more ope

--- src/Leetcode/SearchInsPos35.java
+++ src/Leetcode/SearchInsPos35.java
@@ -4,7 +4,16 @@ public class SearchInsPos35 {
     public static void main(String[] args) {
         int [] nums = {1,3,5,6};
         int target = 5;
+
+        int [] nums1 = {1,3,5,6};
+        int target1 = 2;
+
+        int [] nums2 = {1,3,5,6};
+        int target2 = 7;
+
         System.out.println(searchInsert(nums,target));
+        System.out.println(searchInsert(nums1,target1));
+        System.out.println(searchInsert(nums2,target2));
     }
 
     public static int searchInsert(int[] nums, int target) {
