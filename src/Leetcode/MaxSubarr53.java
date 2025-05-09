0ed80eb89bc73821b9634146f9b5a3e715386038
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2025-05-09 23:59:42 +0530

    more oper

--- src/Leetcode/MaxSubarr53.java
+++ src/Leetcode/MaxSubarr53.java
@@ -3,7 +3,12 @@ package Leetcode;
 public class MaxSubarr53 {
     public static void main(String[] args) {
         int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+        int[] nums1 = {1};
+        int[] nums2 = {5,4,-1,7,8};
+
         System.out.println(maxSubArray(nums));
+        System.out.println(maxSubArray(nums1));
+        System.out.println(maxSubArray(nums2));
     }
     public static int maxSubArray(int[] nums) {
 
